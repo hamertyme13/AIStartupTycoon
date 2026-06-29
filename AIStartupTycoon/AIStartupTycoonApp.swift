939a1b4565555f3b9a7b140162d10bfb987a4fa1
Joshua Hamer <joshuahamer13@gmail.com>
2026-06-29 12:08:00 -0400

    Initial Commit

---
+++ AIStartupTycoon/AIStartupTycoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIStartupTycoonApp.swift
+//  AIStartupTycoon
+//
+//  Created by Joshua Hamer on 6/29/26.
+//
+
+import SwiftUI
+
+@main
+struct AIStartupTycoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
